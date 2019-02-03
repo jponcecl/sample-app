086e960e3f428299ea83c2bb019bc5ccd6de4f55
Juan Ponce <juan.ponce.contreras@gmail.com>
2019-02-02 23:38:04 -0300

    Implementa login basico

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Bienvenido a la App de Ejemplo!"
       redirect_to @user
       #ESTO TB EQUIVALE redirect_to user_url(@user)
